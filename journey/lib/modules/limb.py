35345ad94b04310cabea148c7a3116ee3a600934
nilusss <48320346+nilusss@users.noreply.github.com>
2021-03-08 11:28:21 +0100

    Class Limb - clean up outliner
    reparented nodes for a cleaner outliner in maya

--- journey/lib/modules/limb.py
+++ journey/lib/modules/limb.py
@@ -37,35 +37,40 @@ class Limb(lo.Module):
         self.base_rig = base_rig
 
         # init empty public variables
-
+        self.offset_joint = ''
+        self.ik_joints = []
+        self.fk_joints = []
+        self.blend_ctrl = ''
+        self.arm_ik = ''
+        self.arm_fk = ''
         # init Module class
         #super(Limb, self).__init__(self.prefix, self.base_rig)
-        print("init limb")
 
     def create(self, *args):
         # create module from parent class
         super(Limb, self).create_structure()
 
-        offset_joint = pm.listRelatives(self.driven[0], parent=True)
-        pm.delete(pm.parentConstraint(offset_joint, self.joints_offset_grp, mo=0))
+        self.offset_joint = pm.listRelatives(self.driven[0], parent=True)
+        if self.offset_joint:
+            pm.delete(pm.parentConstraint(self.offset_joint, self.joints_offset_grp, mo=0))
 
-        ik_joints = tools.joint_duplicate(self.driven, 'IK', offset_grp=self.joints_offset_grp)
-        fk_joints = tools.joint_duplicate(self.driven, 'FK', offset_grp=self.joints_offset_grp)
+        self.ik_joints = tools.joint_duplicate(self.driven, 'IK', offset_grp=self.joints_offset_grp)
+        self.fk_joints = tools.joint_duplicate(self.driven, 'FK', offset_grp=self.joints_offset_grp)
 
-        arm_ik = kine.IK(ik_joints, prefix=self.prefix, scale=self.scale, rig_module=self.get_instance())
-        arm_ik.create()
-        arm_ik.pole_vector()
-        arm_fk = kine.FK(fk_joints, prefix=self.prefix, scale=self.scale, rig_module=self.get_instance())
-        arm_fk.create()
+        self.arm_ik = kine.IK(self.ik_joints, prefix=self.prefix, scale=self.scale, rig_module=self.get_instance())
+        self.arm_ik.create()
+        self.arm_ik.pole_vector()
+        self.arm_fk = kine.FK(self.fk_joints, prefix=self.prefix, scale=self.scale, rig_module=self.get_instance())
+        self.arm_fk.create()
 
         # setup stretch if argument is True
         if self.stretch:
-            arm_ik.stretch()
+            self.arm_ik.stretch()
 
-        blend_ctrl = ctrl.Control(prefix=self.prefix + 'IKFKBlend', trans_to=self.driven[-1],
-                                  scale=self.scale, parent=self.controls_grp, shape='cog')
-        blend_ctrl.create()
-        blend_ctrl.get_offset().attr('rx').set(90)
+        self.blend_ctrl = ctrl.Control(prefix=self.prefix + 'IKFKBlend', trans_to=self.driven[-1],
+                                       scale=self.scale, parent=self.controls_grp, shape='cog')
+        self.blend_ctrl.create()
+        self.blend_ctrl.get_offset().attr('rx').set(90)
 
         # get vector between lower and end joint. move blend controller with that offset
         lower_pos = pm.xform(self.driven[-2], q=True, ws=True, t=True)
@@ -79,7 +84,27 @@ class Limb(lo.Module):
         #_pos = get_offset.normal() * get_offset.length() + end_joint_vec
 
         # TODO: position the blend controller using vector position instead of pm.move
-        pm.move(blend_ctrl.get_offset(), get_offset, 0, get_offset, r=True)
-        #pm.move(blend_ctrl.get_offset(), _pos[0], _pos[1], _pos[2])
+        if self.prefix.startswith('l_'):
+            pm.move(self.blend_ctrl.get_offset(), get_offset, 0, -get_offset, r=True)
+        elif self.prefix.startswith('r_'):
+            pm.move(self.blend_ctrl.get_offset(), get_offset, 0, get_offset, r=True)
 
-        tools.joint_constraint(fk_joints, self.driven, blender=blend_ctrl.get_ctrl(), driver2=ik_joints)
+        # setup blending between joint chains and driven. blend_ctrl gets .blend attr added in function
+        if self.blend_ctrl:
+            for driver1, driven, driver2 in zip(self.fk_joints, self.driven, self.ik_joints):
+                tools.matrix_blend(driver1, driven, blender=self.blend_ctrl.get_ctrl(), driver2=driver2, channels=['t', 'r'])
+
+        # setup blend ctrl to hide the kinematic that isn't in use
+        reverse = pm.createNode('reverse', n=self.prefix + 'rev_vis')
+        pm.connectAttr(self.blend_ctrl.get_ctrl() + '.blend', reverse + '.inputX')
+
+        for driven in self.arm_fk.fk_dict.values():
+            pm.connectAttr(reverse + '.outputX', driven.get_offset() + '.v')
+
+        pm.connectAttr(self.blend_ctrl.get_ctrl() + '.blend', self.arm_ik.ik_ctrl.get_offset() + '.v')
+        pm.connectAttr(self.blend_ctrl.get_ctrl() + '.blend', self.arm_ik.pv_ctrl.get_offset() + '.v')
+        pm.connectAttr(self.blend_ctrl.get_ctrl() + '.blend', self.arm_ik.crv_offset + '.v')
+
+        self.blend_ctrl.get_ctrl().attr('blend').set(1)
+
+
