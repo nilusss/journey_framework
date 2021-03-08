5dd6a2c0408ab9800bc85a5c97211fc58c78a196
nilusss <48320346+nilusss@users.noreply.github.com>
2021-03-08 11:25:55 +0100

    Class Arm - added clavicle functionality
    ability to add a clavicle to an arm, added world and clavicle space

--- journey/lib/modules/arm.py
+++ journey/lib/modules/arm.py
@@ -5,9 +5,11 @@ import pymel.core as pm
 import journey.lib.control as ctrl
 import journey.lib.utils.tools as tools
 import journey.lib.modules.limb as li
+import journey.lib.space_switcher as space
 reload(ctrl)
 reload(tools)
 reload(li)
+reload(space)
 import journey.lib.modules.limb as li
 
 
@@ -15,6 +17,7 @@ class Arm(li.Limb):
     def __init__(self,
                  driven,
                  clavicle='',
+                 spaces=[],
                  stretch=True,
                  joint_radius=0.4,
                  prefix='new',
@@ -25,15 +28,43 @@ class Arm(li.Limb):
 
         self.driven = driven
         self.clavicle = clavicle
+        self.spaces = tools.list_check(spaces)
         self.stretch = stretch
         self.joint_radius = joint_radius
         self.prefix = prefix
         self.scale = scale
         self.base_rig = base_rig
-        #super(Arm, self).__init__(driven, stretch, joint_radius, prefix, scale, base_rig)
-        print("init arm")
 
     def create(self):
         super(Arm, self).create()
         if self.clavicle:
-            pass
+            clavicle_ctrl = ctrl.Control(prefix=self.prefix + 'Clavicle', trans_to=self.clavicle,
+                                         rot_to=self.clavicle, scale=self.scale * 0.5,
+                                         parent=self.controls_grp, shape='line_sphere')
+            clavicle_ctrl.create()
+            clavicle_chain = tools.joint_duplicate(joint_chain=[self.clavicle, self.driven[0]],
+                                                   joint_type="FK", offset_grp=self.joints_offset_grp)
+            clavicle_chain[-1].replace('_jnt1', 'Off_jnt')
+
+            tools.matrix_constraint(clavicle_chain[-1], self.ik_joints[0], mo=True)
+
+            clavicle_ctrl.set_constraint(clavicle_chain[0])
+            clavicle_ctrl.set_constraint(self.arm_ik.upper_null)
+
+            clavicle_ctrl.set_constraint(self.arm_fk.fk_dict.values()[0].get_ctrl(), channels=['t'])
+
+            tools.matrix_constraint(clavicle_chain[0], self.clavicle)
+
+            ss_pole = space.SpaceSwitcherLogic(self.arm_ik.ik_ctrl.get_ctrl(), self.arm_ik.pv_ctrl.get_ctrl())
+            ss_pole.setup_switcher()
+
+            ss_ik = space.SpaceSwitcherLogic(clavicle_ctrl.get_ctrl(), self.arm_ik.ik_ctrl.get_ctrl())
+            ss_ik.setup_switcher()
+
+            ss_fk = space.SpaceSwitcherLogic(clavicle_ctrl.get_ctrl(), self.arm_fk.fk_dict.values()[0].get_ctrl())
+            ss_fk.setup_switcher()
+
+            for ss in self.spaces:
+                parent_space = space.SpaceSwitcherLogic(ss, clavicle_ctrl.get_ctrl())
+                parent_space.setup_switcher()
+                parent_space.set_space(ss)
